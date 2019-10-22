50d3742d3236d2c32728280274ae7276ea5f7fd6
Isa Aguilar <isa@aguilarstory.com>
2019-10-21 21:25:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 module "this" {
-	source = "https://github.com/kevinclev/Terraform_create_file.git"
+	source = "https://github.com/kevinclev/Terraform_create_file.git?ref=5668434b4f73ccdc0c62509b78e3a1d3612c4711"
 }
